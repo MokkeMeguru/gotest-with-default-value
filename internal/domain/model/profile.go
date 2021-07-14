db809cae70334180aa828b37062cb81396edfccc
MokkeMeguru <meguru.mokke@gmail.com>
2021-07-14 09:12:54 +0900

    reforamt

--- internal/domain/model/profile.go
+++ internal/domain/model/profile.go
@@ -24,16 +24,16 @@ const (
 )
 
 type Post struct {
-	Position string    `testdata:"beginner"`
-	Field    PostField `testdata:"0"`
+	Position string    `json:"position" testdata:"beginner"`
+	Field    PostField `json:"field" testdata:"0"`
 }
 
 type Profile struct {
-	FirstName     string         `testdata:"Meguru"`
-	LastName      string         `testdata:"Mokke"`
-	FavoriteFoods []Food         `testdata:"[\"curry\", \"ramen\"]"`
-	Skill         map[string]int `testdata:"{\"clojure\": 80, \"golang\": 1}"`
-	Post          Post
+	FirstName     string         `json:"first_name" testdata:"Meguru"`
+	LastName      string         `json:"last_name" testdata:"Mokke"`
+	FavoriteFoods []Food         `json:"favorite_foods" testdata:"[\"curry\", \"ramen\"]"`
+	Skill         map[string]int `json:"skill" testdata:"{\"clojure\": 80, \"golang\": 1}"`
+	Post          *Post          `json:"post" testdata:"{}"`
 }
 
 func (m Profile) Validate() error {
@@ -42,6 +42,9 @@ func (m Profile) Validate() error {
 	}
 	if m.FirstName != "Meguru" {
 		return xerrors.New("who are you ?")
+	}
+	if m.Post == nil {
+		return xerrors.New("no post?")
 	}
 	for language, level := range m.Skill {
 		switch language {
